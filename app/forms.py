ecb7d70a63b8c33be24a6b8bd08e47620029ac72
David Lavelle <dlavelle7@yahoo.ie>
2016-01-13 20:26:39 +0000

    added register route, view and form.

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,15 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, BooleanField, PasswordField, TextField
+from wtforms.validators import DataRequired, EqualTo
 
 
 class LoginForm(Form):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
+
+
+class RegisterForm(Form):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(),
+                    EqualTo('repeat_pass', message="Passwords don't match")])
+    repeat_pass = PasswordField('password', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
 from app import app
 from flask import render_template, flash, redirect
-from app.forms import LoginForm
+from app.forms import LoginForm, RegisterForm
 
 @app.route('/')
 def index():
-    return render_template('index.html', name="David", title="Home")
+    return render_template('index.html', title="Home")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -14,3 +14,12 @@ def login():
             form.username.data, form.password.data))
         return redirect('/')
     return render_template('login.html', title="Sign In", form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        flash("Register data: {0}, {1}".format(
+            form.username.data, form.password.data))
+        return redirect('/')
+    return render_template('register.html', title="Sign Up", form=form)
