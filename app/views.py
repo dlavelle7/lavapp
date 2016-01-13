8866afb2e2734bb78b580a63275efbfc886feb46
David Lavelle <dlavelle7@yahoo.ie>
2016-01-13 19:21:59 +0000

    Basic forms.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 @app.route('/')
 def index():
-    return render_template('index.html', name="David", title="lavapp")
+    return render_template('index.html', name="David", title="Home")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login data: {0}".format(form.login.data))
+        return redirect('/')
+    return render_template('login.html', title="Sign In", form=form)
