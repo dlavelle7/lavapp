29f4fec7c22908ff3256275b6095f226acafa4df
David Lavelle <dlavelle7@yahoo.ie>
2016-01-23 16:25:42 +0000

    added flask admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 
@@ -8,5 +10,7 @@ db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
+admin = Admin(app, name='lavapp', template_mode='bootstrap3')
 
 from app import views, models
+admin.add_view(ModelView(models.User, db.session))
