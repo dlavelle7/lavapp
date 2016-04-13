59f3268e1690625d194ae50d3460551027ecee70
David Lavelle <dlavelle7@yahoo.ie>
2016-04-13 20:11:36 +0100

    email not unique.

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(64))
-    email = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(64))
 
     def __init__(self, username, password, email):
         self.username = username
