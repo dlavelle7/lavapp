c1eba182ce84995d073e27c3c26303d22e760879
David Lavelle <dlavelle7@yahoo.ie>
2016-08-03 13:39:42 +0100

    unit tests passing again.

--- tests/test_budget.py
+++ tests/test_budget.py
@@ -1,5 +1,5 @@
 from app import db
-from app.models import User, Income, Expense, round_for_currency, Budget
+from app.models import Income, Expense, round_for_currency, Budget
 import base_test
 import datetime
 from decimal import Decimal
@@ -8,18 +8,11 @@ from decimal import Decimal
 class TestBudget(base_test.BaseTest):
 
     def test_budget(self):
-        # Create a new user
-        user = User(username='john', password='pass', email='john@foo.com')
-        db.session.add(user)
-        db.session.commit()
-
-        # Create a new budget for user john
-        budget = Budget("Plan-A", user.id)
+        # Create a new budget budget 
+        budget = Budget("Plan-A", 1)
         db.session.add(budget)
         db.session.commit()
         self.assertEqual("Plan-A", budget.name)
-        self.assertEqual(budget.user, user)
-        self.assertEqual(budget.user_id, user.id)
 
         # Create an Income for the budget
         income = Income(name='salary', amount=Decimal('4321.09'),
@@ -28,7 +21,6 @@ class TestBudget(base_test.BaseTest):
         db.session.commit()
         self.assertTrue(income in budget.incomes)
         self.assertEqual(income.budget, budget)
-        self.assertEqual(income.budget_id, budget.id)
 
         # Create an Expense for the budget
         expense = Expense(name='rent', amount=Decimal(1250.0), budget_id=budget.id,
@@ -41,3 +33,52 @@ class TestBudget(base_test.BaseTest):
         db.session.add(expense2)
         db.session.commit()
         self.assertEqual(sorted([expense, expense2]), sorted(budget.expenses))
+
+    def test_total_income(self):
+        budget = Budget("Plan-A", 1)
+        db.session.add(budget)
+        db.session.commit()
+
+        income = Income(name='salary', amount=Decimal(100), budget_id=budget.id,
+                interval='weekly')
+        db.session.add(income)
+        income2 = Income(name='investment', amount=Decimal(555),
+                budget_id=budget.id, interval='monthly')
+        db.session.add(income2)
+        db.session.commit()
+        self.assertEqual('955.00', budget.total_income)
+
+        db.session.delete(income2)
+        db.session.commit()
+        self.assertEqual('400.00', budget.total_income)
+
+    def test_balance(self):
+        budget = Budget("Plan-A", 1)
+        db.session.add(budget)
+        db.session.commit()
+
+        income = Income(name='salary', amount=Decimal(100), budget_id=budget.id,
+                interval='weekly')
+        db.session.add(income)
+        income2 = Income(name='investment', amount=Decimal(555),
+                budget_id=budget.id, interval='monthly')
+        db.session.add(income2)
+        db.session.commit()
+        self.assertEqual('955.00', budget.total_income)
+        self.assertEqual('955.00', budget.balance)
+
+        expense = Expense(name='rent', amount=Decimal(1250.0), budget_id=budget.id,
+                interval='monthly', shared_by=2)
+        self.assertEqual(expense.total, 625.0)
+        db.session.add(expense)
+        db.session.commit()
+
+        self.assertEqual('330.00', budget.balance)
+
+        expense2 = Expense(name='UPC', amount=Decimal(60.0), budget_id=budget.id,
+                interval='monthly', shared_by=2)
+        self.assertEqual(expense2.total, 30.0)
+        db.session.add(expense2)
+        db.session.commit()
+
+        self.assertEqual('300.00', budget.balance)
